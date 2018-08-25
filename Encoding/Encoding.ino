6f682357445d770e3339fb61980d34d8b219bef4
Equinox <scarras86@gmail.com>
2018-08-25 13:06:40 -0400

    Initializing the git repo

---
+++ Encoding/Encoding.ino
@@ -0,0 +1,15 @@
+//The idea here is to create an encoder for a handhel arpeggiator
+// the inputs are : 
+  // - the base key
+  // - major/minor scaling
+  // 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
